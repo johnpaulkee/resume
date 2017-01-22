4c5a3b7818c51012370e5dedf28675aa1f03c675
John Paul Kee <johnpaulkee@gmail.com>
2017-01-22 00:43:55 -0800

    Hello world

--- app/src/App.js
+++ app/src/App.js
@@ -13,6 +13,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>
+            Hello World, my name is JP
+        </p>
       </div>
     );
   }
